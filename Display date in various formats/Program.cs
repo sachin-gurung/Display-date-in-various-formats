910739f43ef609ce20b67c9ce58b6dfaf638e417
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:43:45 -0800

    First Commit

--- Display date in various formats/Program.cs
+++ Display date in various formats/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Display_date_in_various_formats
+﻿/*
+ * C# Program to Display the Date in Various Formats 
+ */
+using System;
+namespace DateAndTime
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main()
         {
+            DateTime date = new DateTime(2013, 6, 23);
+            Console.WriteLine("Some Date Formats : ");
+            Console.WriteLine("Date and Time:  {0}", date);
+            Console.WriteLine(date.ToString("yyyy-MM-dd"));
+            Console.WriteLine(date.ToString("dd-MMM-yy"));
+            Console.WriteLine(date.ToString("M/d/yyyy"));
+            Console.WriteLine(date.ToString("M/d/yy"));
+            Console.WriteLine(date.ToString("MM/dd/yyyy"));
+            Console.WriteLine(date.ToString("MM/dd/yy"));
+            Console.WriteLine(date.ToString("yy/MM/dd"));
+            Console.Read();
+            return 0;
         }
     }
 }
+/*
+Some Date Formats :
+Date and Time:  6/23/2013 12:00:00 AM
+2013-06-23
+23-Jun-13
+6/23/2013
+6/23/13
+06/23/2013
+06/23/13
+13/06/23
+
+Press any key to continue . . .
+*/
